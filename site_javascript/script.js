58b29267cb586039ceff76c6d1b3300270f7d3a5
BERNARDO GIRARDI FRIGERI <03534416007@senacrs.edu.br>
2025-09-23 09:25:13 -0300

    Atualização de conteúdo

--- site_javascript/script.js
+++ site_javascript/script.js
@@ -1,3 +1,8 @@
-var nome = "Bernardo Girardi Frigeri";
-var ano = 2025;
-alert("Primeiro Ano Senac - " + nome + " - " + ano);
+function imprimirVariaveis(){
+// var ano = 2025;
+// var ensino = "Primeiro Ano Senac";
+var nome = document.getElementById("nome").value;
+var sobrenome = document.getElementById("sobrenome").value;
+var endereco = document.getElementById("endereco").value;
+alert(nome + " " + sobrenome + " " + endereco);
+}
