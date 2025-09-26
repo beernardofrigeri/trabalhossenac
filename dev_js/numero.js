9fe0b98bc7749ccb914d7918fbef64ffe15d9589
BERNARDO GIRARDI FRIGERI <03534416007@senacrs.edu.br>
2025-09-26 12:15:33 -0300

    Atualização de conteúdo

---
+++ dev_js/numero.js
@@ -0,0 +1,6 @@
+function recebeNumeros(){
+    var n1 = parseInt(document.getElementById("n1").value);
+    var n2 = parseInt(document.getElementById("n2").value);
+    var soma = n1 + n2;
+    alert("A soma é: " + soma);
+}
