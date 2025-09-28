65c72a898cc3ef3c19eb35b320af806c74dd0a63
Bernardo <be.frigeri@gmail.com>
2025-09-28 09:16:37 -0300

    atualização

--- site_javascript/script.js
+++ site_javascript/script.js
@@ -1,8 +1,9 @@
 function imprimirVariaveis(){
 // var ano = 2025;
 // var ensino = "Primeiro Ano Senac";
-var nome = document.getElementById("nome").value;
-var sobrenome = document.getElementById("sobrenome").value;
-var endereco = document.getElementById("endereco").value;
-alert("Nome: " + nome + "\nSobrenome: " + sobrenome + "\nEndereço: " + endereco);
+var usuario = document.getElementById("Usuário").value;
+var senha = document.getElementById("Senha").value;
+var confirmacao = document.getElementById("Confirmação da Senha").value;
+
+alert("Usuário: " + usuario + "\nSenha: " + senha + "\nConfirmação da Senha: " + confirmacao);
 }
