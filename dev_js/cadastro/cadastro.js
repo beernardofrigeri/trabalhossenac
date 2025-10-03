621f2b3b587555bbcc6509da79c58bae4991564e
BERNARDO GIRARDI FRIGERI <03534416007@senacrs.edu.br>
2025-10-03 12:18:56 -0300

    Atualização de conteúdo

---
+++ dev_js/cadastro/cadastro.js
@@ -0,0 +1,9 @@
+function imprimirValores() {
+    var nome = document.getElementById("nome").value;
+    var nascimento = document.getElementById("nascimento").value;
+    var sexo = document.getElementById("sexo").value;
+    var estado_civil = document.getElementById("estado_civil").value;
+    var escolaridade = document.getElementById("escolaridade").value;
+    
+    alert("Nome: " + nome + "\nNascimento: " + nascimento + "\nSexo: " + sexo + "\nEstado Civil: " + estado_civil + "\nEscolaridade: " +escolaridade);
+}
