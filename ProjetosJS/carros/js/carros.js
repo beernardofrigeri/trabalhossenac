da301582d10d801426c9079230b46dc4f79c8033
Bernardo <be.frigeri@gmail.com>
2025-10-04 16:23:52 -0300

    Atualização geral: substituição de pastas e arquivos reorganizados

---
+++ ProjetosJS/carros/js/carros.js
@@ -0,0 +1,12 @@
+function carro1() {
+    document.getElementById('carro').src = "imagens/porsche.png";
+}
+function carro2() {
+    document.getElementById('carro').src = "imagens/ferrari.png";
+}
+function carro3() {
+    document.getElementById('carro').src = "imagens/lamborghini.png";
+}
+function carro4() {
+    document.getElementById('carro').src = "imagens/mercedes.png";
+}
--- dev_js/lampada/lampada.js
+++
@@ -1,6 +0,0 @@
-function ligar() {
-    var lampada = document.getElementById('lampada').src = "1.jpg";
-}
-function desligar() {
-    var lampada = document.getElementById('lampada').src = "2.png";
-}
